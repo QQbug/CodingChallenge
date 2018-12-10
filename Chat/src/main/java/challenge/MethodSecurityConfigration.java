0528c97712d4b0d2882d592644c17bf20d397966
Qi Wang <wq86428@gmail.com>
2018-12-09 23:23:14 -0500

    Remove Chat.

--- Chat/src/main/java/challenge/MethodSecurityConfigration.java
+++
@@ -1,9 +0,0 @@
-package challenge;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-
-@Configuration
-//@EnableGlobalMethodSecurity(securedEnabled = true)
-public class MethodSecurityConfigration {
-}
